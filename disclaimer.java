1869637a84e054326c285ff01d3a92318e235cdd
LeoAristarco <leoceit@gmail.com>
2019-10-20 16:20:10 -0300

    Update disclaimer.java

--- disclaimer.java
+++ disclaimer.java
@@ -1,1 +1,17 @@
-dar de alta el despachenote si no existe (si no hay un despachenote que tenga ese delivery hay que darlo de alta, la primera vez no va existir el depachenote, le ponemos id_purcheorder y deliveryid), y dar de alta un despachenotedetail con el despachenote que genere y el id_producto) y insertar todos lo epcs que se les estan pasando en el array en esta tabla joinando con la tabla rfid_tag) rfid_tag es la tabla donde se guardan todos los epc hay una columna que se llama epc y hay que hacer ese join.
+Android- prume (similar al caso de uso de recepcion)
+Nosotros tenemos lo que preparan en el CD y lo que preparan en la tienda.
+ Tienen el problema ellos que a veces le dicen de la tienda che vengo a buscar tal pedido y no lo tengo o no saben que paso o tienen algún problema.
+ El cliente en su momento había pedido hacer una auditoria de e-commerce, que entres desde el menu y que levante todos los pedidos que están pendientes de pick-up o recepcionados para ese local, entonces te aparece en pantalla pedido tanto.. tanto.. tanto.. y cada pedido cuantos epc tiene, apretas leer y esperas leer esos epcs (yo conozco los epcs que tengo que leer) voy marcando y los que no están.. después vemos.
+
+ lucho le pregunta a marian que impacto tendría bajar los epcs de todos los pedidos (solo de e-ecomerce) de todos los locales.
+
+
+ Lo que hay que hacer es que cuando entras al menu hay que bajar todos los pedidos de e-commerce cuyo estado sean pendientes de pick-up o recepcionados
+ Es similar a recepción (conozco los epcs que tengo que leer), donde tengo el numero de pedido, en vez de cantidad de cajas, seria cantidad de pedidos, tendría por ejemplo 5/0 productos leídos.
+ cuando haces click en un pedido te muestra cual leyó y cual no,
+
+ Posible inconveniente:
+
+ Ponele que tenía que leer 5 y leiste un pedido, un epc que no era para este local, entonces se pinta en rojo ese número de pedido, en recepción te aparece "como caja de otro local" a aca le tenemos que poner "pedido de otro local"
+
+ Despues en configuracion hay que hacer la subida de esta info que depues vemos que subimos porque hay que pensar más el reporte 
