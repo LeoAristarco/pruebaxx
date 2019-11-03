f56b947afa9b338d49fbe78395c296e089ed2e43
LeoAristarco <leoceit@gmail.com>
2019-11-03 15:39:26 -0300

    Update disclaimer.java

--- disclaimer.java
+++ disclaimer.java
@@ -1,17 +1,12 @@
-Android- prume (similar al caso de uso de recepcion)
-Nosotros tenemos lo que preparan en el CD y lo que preparan en la tienda.
- Tienen el problema ellos que a veces le dicen de la tienda che vengo a buscar tal pedido y no lo tengo o no saben que paso o tienen algún problema.
- El cliente en su momento había pedido hacer una auditoria de e-commerce, que entres desde el menu y que levante todos los pedidos que están pendientes de pick-up o recepcionados para ese local, entonces te aparece en pantalla pedido tanto.. tanto.. tanto.. y cada pedido cuantos epc tiene, apretas leer y esperas leer esos epcs (yo conozco los epcs que tengo que leer) voy marcando y los que no están.. después vemos.
+Para el modulo de recepcion:
 
- lucho le pregunta a marian que impacto tendría bajar los epcs de todos los pedidos (solo de e-ecomerce) de todos los locales.
+Todos estan pendientes de recepcion, los pedidos de ecomerce son los que tenemos problemas (porque los usuarios se pierden)
+hacer 3 pantallas:
+-una que aparezcan solo los de commerces
+-una los de qe o cd
+-en la otro todo lo que sea locales
+(lucho dice que estaria bueno poner las cantidades de cajas en cada uno de esos filtros)
 
+Ponerlos como 3 puntos en el menu principal
 
- Lo que hay que hacer es que cuando entras al menu hay que bajar todos los pedidos de e-commerce cuyo estado sean pendientes de pick-up o recepcionados
- Es similar a recepción (conozco los epcs que tengo que leer), donde tengo el numero de pedido, en vez de cantidad de cajas, seria cantidad de pedidos, tendría por ejemplo 5/0 productos leídos.
- cuando haces click en un pedido te muestra cual leyó y cual no,
-
- Posible inconveniente:
-
- Ponele que tenía que leer 5 y leiste un pedido, un epc que no era para este local, entonces se pinta en rojo ese número de pedido, en recepción te aparece "como caja de otro local" a aca le tenemos que poner "pedido de otro local"
-
- Despues en configuracion hay que hacer la subida de esta info que depues vemos que subimos porque hay que pensar más el reporte 
+fede dice de pasarle un parametro en el intent
